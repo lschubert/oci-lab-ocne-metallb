eb19188dfc4d7c63c24bfe6bdbad2e5f8610efe5
Lars Schubert <Lars.Schubert@oracle.com>
2022-09-06 14:34:26 +0000

    added local only setup to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 export ANSIBLE_HOST_KEY_CHECKING=False
 export ANSIBLE_STDOUT_CALLBACK=debug
-ansible-playbook setup.yml
-ansible-playbook setup.yml -i hosts
+if [[ $# == 1 ]] && [[ $1 == "local" ]]; then
+  ansible-playbook setup.yml
+else
+  ansible-playbook setup.yml
+  ansible-playbook setup.yml -i hosts
+fi
